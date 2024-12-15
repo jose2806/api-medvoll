db4103e7bfc8f1f060c02a3f8d29c0a10add5d91
jose2806 <jose12.jr610@gmail.com>
2024-12-15 15:18:36 -0600

    update AutenticacionService and MedicoController

--- src/main/java/med/voll/api/infra/security/AutenticacionService.java
+++ src/main/java/med/voll/api/infra/security/AutenticacionService.java
@@ -15,6 +15,7 @@ public class AutenticacionService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usuarioRepository.findByUsername(username);
+        return usuarioRepository.findByLogin(username);
     }
+
 }
